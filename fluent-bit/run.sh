4eddb235f52ebefe48babf546f7a03f5eee611a7
rolll13 <35044942+rolll13@users.noreply.github.com>
2025-09-14 13:58:53 +0000

    update start

--- fluent-bit/run.sh
+++ fluent-bit/run.sh
@@ -3,36 +3,36 @@ set -e
 
 CONFIG_PATH=/data/options.json
 
-# читаем значения из options.json
 LOG_LEVEL=$(jq -r '.log_level' $CONFIG_PATH)
 OUTPUT=$(jq -r '.output' $CONFIG_PATH)
 PARSERS=$(jq -r '.parsers' $CONFIG_PATH)
 
-# генерируем fluent-bit.conf
+# Создаём конфиги
 cat > /data/fluent-bit.conf <<EOF
 [SERVICE]
-    Flush        5
+    Flush        1
     Daemon       Off
     Log_Level    ${LOG_LEVEL}
     Parsers_File /data/parsers.conf
 
 [INPUT]
-    Name tail
-    Path /config/home-assistant.log
-    Parser docker
-    Tag homeassistant
+    Name   tail
+    Path   /config/home-assistant.log
+    Tag    hass
+    Parser json
+    DB     /data/flb_hass.db
 
 [INPUT]
-    Name tail
-    Path /data/supervisor/home-assistant.log
-    Parser docker
-    Tag supervisor
+    Name   tail
+    Path   /data/supervisor/home-assistant.log
+    Tag    supervisor
+    Parser json
+    DB     /data/flb_supervisor.db
 
 ${OUTPUT}
 EOF
 
-# генерируем parsers.conf
 echo "${PARSERS}" > /data/parsers.conf
 
-# запускаем Fluent Bit
-exec /usr/bin/fluent-bit -c /data/fluent-bit.conf
+echo "[INFO] Starting Fluent Bit with log level: ${LOG_LEVEL}"
+exec /usr/local/fluent-bit/bin/fluent-bit -c /data/fluent-bit.conf
